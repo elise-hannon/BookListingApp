ced834bb7945743c431b52f6a4604fcfa7bfd41e
Elise <hannon.elise@gmail.com>
2018-05-06 14:24:12 -0400

    parsed JSON using Gson

---
+++ app/src/main/java/com/example/android/booklistingapp/models/BookResponseItem.java
@@ -0,0 +1,13 @@
+package com.example.android.booklistingapp.models;
+
+public class BookResponseItem {
+    private VolumeInfo volumeInfo;
+
+    public VolumeInfo getVolumeInfo() {
+        return volumeInfo;
+    }
+
+    public void setVolumeInfo(VolumeInfo volumeInfo) {
+        this.volumeInfo = volumeInfo;
+    }
+}
